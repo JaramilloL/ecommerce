6d6c034cdbc54fc89061516665fa74e6684a3cf1
JLJL <juanluisjaramillolopez@gmail.com>
2024-03-08 15:21:41 -0600

    creacion basica de lso archivos necesarios para compartir datos de firebase

--- src/context/ReducerContext.jsx
+++ src/context/ReducerContext.jsx
@@ -1,7 +1,18 @@
-const ReducerContext = () => {
-  return (
-    <div>ReducerContext</div>
-  )
+const ReducerContext = (state, action) => {
+  switch(action.type) {
+    case 'login':
+        return {
+            ...state,
+            isAuthenticated: true,
+            username: action.payload
+        }
+    case 'logout':
+        return {
+            ...state,
+            isAuthenticated: false,
+            username: null
+        }
+  }
 }
 
 export default ReducerContext
--- src/context/StateContext.jsx
+++ src/context/StateContext.jsx
@@ -1,7 +1,30 @@
-const StateContext = () => {
+import { useReducer } from 'react'
+import { UserContext } from '../context/UserContext'
+import PropTypes from 'prop-types'
+import ReducerContext from './ReducerContext'
+
+const StateContext = ({ children }) => {
+    //creamos un estado inicial de la app
+    const initialState ={
+        isAuthenticated: false,
+        username: null,
+    }
+
+    //creamos un estado inicial en el reducer para controlar el estado de logeo de la app
+    const [state, dispatch] = useReducer(ReducerContext, initialState)
   return (
-    <div>StateContext</div>
+
+    <UserContext.Provider value={{
+        state,
+        dispatch
+    }}>
+        {children}
+    </UserContext.Provider>
   )
 }
 
 export default StateContext
+
+StateContext.propTypes = {
+    children: PropTypes.any
+}
